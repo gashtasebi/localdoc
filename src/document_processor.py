c2ecd141e021e80882f62b09c0de442f545a8635
Abed Gashtasebi <abed.gashtasebi@gmail.com>
2026-08-30 19:11:02 +0200

    refactor: use document models in prossing pipeline

--- src/document_processor.py
+++ src/document_processor.py
@@ -1,9 +1,15 @@
+from src.models import Page, Document
 from src.pdf_parser import extract_text_from_pdf
 from src.text_cleaner import clean_text
 
 def process_pdf(pdf_path):
     pages = extract_text_from_pdf(pdf_path)
 
-    for page in pages:
-        page["text"] = clean_text(page["text"])
-    return pages
+    page_objects = [
+        Page(
+            page_number = page["page_number"],
+            text = clean_text(page["text"])
+        )
+        for page in pages
+    ]
+    return Document(pages= page_objects)
--- tests/test_document_processor.py
+++ tests/test_document_processor.py
@@ -1,6 +1,8 @@
 import pymupdf
 
+
 from src.document_processor import process_pdf
+from src.models import Document
 
 def test_process_pdf(tmp_path):
     pdf_path = tmp_path / "test_document.pdf"
@@ -14,10 +16,10 @@ def test_process_pdf(tmp_path):
     document.save(pdf_path)
     document.close()
 
-    pages = process_pdf(pdf_path)
+    result = process_pdf(pdf_path)
 
-
-    assert len(pages) == 1
-    assert pages[0]["page_number"] == 1
-    assert "Frequenzumrichter" in pages[0]["text"]
-    assert "Maschine M42" in pages[0]["text"]
+    assert isinstance(result, Document)
+    assert len(result.pages) == 1
+    assert result.pages[0].page_number == 1
+    assert "Frequenzumrichter" in result.pages[0].text
+    assert "Maschine M42" in result.pages[0].text
